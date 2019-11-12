7fe26a10dba462f8798b6bc3280058ab95c5a73f
Muhammad Zeshan Arif <>
2019-11-13 03:57:39 +0500

    This is my first commit

---
+++ apps/accounts/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Partner(models.Model):
+    """
+    This model represents white-labelled partners.
+    """
+    label = models.CharField(max_length=100)
+    main_logo = models.CharField(max_length=255)
+    small_logo = models.CharField(max_length=255)
+    slug = models.CharField(max_length=100, unique=True)
+
+    def __unicode__(self):
+        return '{}'.format(self.label)
+
+    class Meta:
+        verbose_name = "Partner"
+        verbose_name_plural = "Partners"
