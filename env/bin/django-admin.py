5797ac790434a2f9c91cbe979a2c721fe611dfdf
Muhammad Zeshan Arif <>
2019-11-13 11:57:40 +0500

    test pylint prehook

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/zeeshanarif/workspace/the-lab/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ test_pylint/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class TestPylintConfig(AppConfig):
+    name = 'test_pylint'
